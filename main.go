466ac8bfbba1d6b45ea6025080166f410fb7f201
Jason <106798996+JasonL111@users.noreply.github.com>
2025-04-01 18:49:13 +1100

    Update the main Go code

--- main.go
+++ main.go
@@ -1,84 +1,161 @@
-package main
+// This program was modified by JasonL111 in 2024
+// Base on https://github.com/awsdocs/aws-doc-sdk-examples
 
-import (
-  "fmt"
-  "os"
-  "strconv"
-  "time"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/credentials"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/s3"
-  "github.com/joho/godotenv"
-)
+/*
+ * Copyright 2024 JasonL111
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-func main() {
-  err := godotenv.Load()
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "failed to load .env file:", err)
-  }
+ package main
 
-  newSession, err := session.NewSession(&aws.Config{
-    Credentials: credentials.NewStaticCredentials(
-      os.Getenv("S3_KEY_ID"),
-      os.Getenv("S3_SECRET"),
-      "",
-    ),
-    Region:           aws.String(os.Getenv("S3_REGOIN")),
-    Endpoint:         aws.String(os.Getenv("S3_ENDPOINT")),
-    S3ForcePathStyle: aws.Bool(true),
-  })
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "failed to create session:", err)
-    return
-  }
-
-  bucket := os.Getenv("S3_BUCKET")
-  prefix := os.Getenv("S3_PREFIX")
-  durationHours, err := strconv.Atoi(os.Getenv("S3_DURATION_HOURS"))
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "S3_DURATION_HOURS is not an integer:", err)
-    return
-  }
-
-  client := s3.New(newSession)
-
-  var continuationToken *string = nil
-
-  for {
-    listObjectsV2Output, err := client.ListObjectsV2(&s3.ListObjectsV2Input{
-      ContinuationToken: continuationToken,
-      Prefix:            aws.String(prefix),
-      Bucket:            aws.String(bucket),
-    })
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "failed to list objects:", err)
-      return
-    }
-
-    for _, object := range listObjectsV2Output.Contents {
-      getObjectReq, _ := client.GetObjectRequest(&s3.GetObjectInput{
-        Key:    object.Key,
-        Bucket: aws.String(bucket),
-      })
-
-      presign, err := getObjectReq.Presign(time.Hour * time.Duration(durationHours))
-      if err != nil {
-        panic(err)
-      }
-      fmt.Println(*object.Key)
-      fmt.Println()
-      fmt.Println(presign)
-      fmt.Println()
-    }
-
-    if !(*listObjectsV2Output.IsTruncated) {
-      // all objects are listed
-      return
-    }
-
-    continuationToken = listObjectsV2Output.ContinuationToken
-  }
-
-}
+ import (
+   "context"
+   "fmt"
+   "log"
+   "os"
+   "time"
+ 
+   "github.com/aws/aws-sdk-go-v2/aws"
+   "github.com/aws/aws-sdk-go-v2/config"
+   "github.com/aws/aws-sdk-go-v2/credentials"
+   "github.com/aws/aws-sdk-go-v2/service/s3"
+   "github.com/joho/godotenv"
+ )
+ 
+ // Presigner encapsulates the Amazon S3 presign actions.
+ type Presigner struct {
+   PresignClient *s3.PresignClient
+ }
+ 
+ // GetObject generates a presigned URL for downloading an object from S3-compatible storage.
+ func (presigner Presigner) GetObject(
+   ctx context.Context, bucketName string, objectKey string, lifetimeSecs int64) (string, error) {
+   request, err := presigner.PresignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+     Bucket: aws.String(bucketName),
+     Key:    aws.String(objectKey),
+   }, func(opts *s3.PresignOptions) {
+     opts.Expires = time.Duration(lifetimeSecs) * time.Second
+   })
+   if err != nil {
+     log.Printf("Couldn't get a presigned request for %v:%v. Here's why: %v\n",
+       bucketName, objectKey, err)
+     return "", err
+   }
+   return request.URL, nil
+ }
+ 
+ func main() {
+   err := godotenv.Load()
+   if err != nil {
+     log.Fatalf("Error loading .env file: %v", err)
+   }
+   // Backblaze B2 credentials
+   b2KeyID := os.Getenv("B2_KEY_ID")
+   b2ApplicationKey := os.Getenv("B2_APPLICATION_KEY")
+   b2Endpoint := os.Getenv("B2_ENDPOINT")
+   b2Region := os.Getenv("B2_REGION")
+   bucketName := os.Getenv("B2_BUCKET_NAME")
+   prefix := os.Getenv("PREFIX")
+   lifetimeSecs := int64(3600) // Presigned URL valid for 1 hour
+ 
+   // Check for required environment variables
+   if b2KeyID == "" || b2ApplicationKey == "" || b2Endpoint == "" || b2Region == "" || bucketName == "" {
+     log.Fatal("Missing required environment variables.")
+   }
+ 
+   // AWS Config with B2 credentials and custom endpoint
+   cfg, err := config.LoadDefaultConfig(context.TODO(),
+     config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+       b2KeyID,
+       b2ApplicationKey,
+       "", // Session token is not used for Backblaze B2
+     )),
+     config.WithRegion(b2Region),
+     config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+       func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+         if service == s3.ServiceID {
+           return aws.Endpoint{
+             URL:           b2Endpoint,
+             SigningRegion: b2Region,
+           }, nil
+         }
+         return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
+       })),
+   )
+   if err != nil {
+     log.Fatalf("Failed to load configuration: %v", err)
+   }
+ 
+   // Initialize S3 and Presign clients with UsePathStyle option
+   s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+     o.UsePathStyle = true
+   })
+   presignClient := s3.NewPresignClient(s3Client)
+   presigner := Presigner{PresignClient: presignClient}
+ 
+   // List objects in the bucket with optional prefix
+   listObjectsInput := &s3.ListObjectsV2Input{
+     Bucket: aws.String(bucketName),
+   }
+   if prefix != "" {
+     listObjectsInput.Prefix = aws.String(prefix)
+   }
+ 
+   // Open file to save URLs
+   outputFile := "presigned_urls.txt"
+   file, err := os.Create(outputFile)
+   if err != nil {
+     log.Fatalf("Failed to create output file: %v", err)
+   }
+   defer file.Close()
+ 
+   // Paginated listing and URL generation
+   for {
+     output, err := s3Client.ListObjectsV2(context.TODO(), listObjectsInput)
+     if err != nil {
+       log.Fatalf("Failed to list objects in bucket %s: %v", bucketName, err)
+     }
+ 
+     for _, item := range output.Contents {
+       // Generate a Presigned URL for each object
+       url, err := presigner.GetObject(context.TODO(), bucketName, *item.Key, lifetimeSecs)
+       if err != nil {
+         log.Printf("Failed to generate presigned URL for object %s: %v", *item.Key, err)
+         continue
+       }
+ 
+       // Write object key and URL to file
+       _, err = file.WriteString("Object Key: " + *item.Key + "\n")
+       if err != nil {
+         log.Fatalf("Failed to write to file: %v", err)
+       }
+       _, err = file.WriteString("Presigned URL: " + url + "\n\n")
+       if err != nil {
+         log.Fatalf("Failed to write to file: %v", err)
+       }
+     }
+ 
+     // If no more pages, break the loop
+     if output.IsTruncated == nil || !*output.IsTruncated {
+       break
+     }
+ 
+     // Set continuation token for next page
+     listObjectsInput.ContinuationToken = output.NextContinuationToken
+   }
+ 
+   log.Printf("Presigned URLs have been written to %s", outputFile)
+ }
+ 
